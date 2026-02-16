62b04f6166fd15bda7ce057e61cc49490ea6ff68
zyhwan <145274494+zyhwan@users.noreply.github.com>
2026-02-16 19:31:08 +0900

    전방 선언
    전방 선언의 필요 이유 : 헤더파일의 선언부에서 불필요한 헤더를 포함시키지 않고 파일크기를 줄여서 컴파일 시간을 줄일 수 있다. Cpp 파일에서만 헤더를 포함시킬 것.

--- enc_temp_folder/2efeb313523d5144b5783c4914a52882/BasePawn.h
+++ enc_temp_folder/2efeb313523d5144b5783c4914a52882/BasePawn.h
@@ -19,10 +19,6 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-private:
-	UPROPERTY()
-	class UCapsuleComponent* CapsuleComp;
-
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
