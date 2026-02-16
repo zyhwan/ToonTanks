b08866577c3c23f41be0d7d29b22d4effdacc517
zyhwan <145274494+zyhwan@users.noreply.github.com>
2026-02-16 23:23:44 +0900

    BasePawn 기반 자식 클래스 생성
    BasePawn 기반 자식 클래스 생성 및 PROPERTY() 메크로 설명
    
    PROPERTY()의 메크로 종류
    VisibleAnywhere : 읽기만 가능. 편집x
    EditAnywhere : 읽기, 쓰기 모두 가능. 편집o
    VisibleInstanceOnly : 읽기 가능. 디폴트 블루프린트에서는 확인할 수 없고 레벨에 인스턴스로 존재하는 디테일패널에서 읽기 가능.
    VisibleDefaultsOnly : 디폴트 블루프린트에서만 읽기 가능.
    EditDefaultsOnly : 디폴트 블루프린트에서만 읽기, 쓰기 가능.
    EditInstanceOnly : 인스턴스에서만 읽기, 쓰기 가능.
    BlueprintReadWrite : 이벤트 그래프에서 호출 및 편집 가능.
    BlueprintReadOnly : 이벤트 그래프에서 호출 가능, 편집x.
    
    PROPERTY()의 메크로 특징
    읽기 및 쓰기 액세스가 있고, private 섹션에서는 사용할 수 없다.
    private 지정자에서 데이터를 읽거나 쓰고 싶은경우 인자로 meta = (AllowPrivateAccess = "true")를 넣으면 사용가능하다.
    Category를 넣을 수 있는데 [Category = "Category Name"] 이런식으로 인자값을 넣어주면 블루프린트 에디터에서 해당 카테고리 이름으로 구분이 생긴다.

--- Source/ToonTanks/BasePawn.h
+++ Source/ToonTanks/BasePawn.h
@@ -20,16 +20,16 @@ protected:
 	virtual void BeginPlay() override;
 
 private:
-	UPROPERTY()
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	class UCapsuleComponent* CapsuleComp;
 
-	UPROPERTY()
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	UStaticMeshComponent* BaseMesh;
 
-	UPROPERTY()
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	UStaticMeshComponent* TurretMesh;
 
-	UPROPERTY()
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	USceneComponent* ProjectileSpawnPoint;
 
 public:	
@@ -43,8 +43,25 @@ public:
 	// VisibleAnywhere : 읽기만 가능. 편집x 
 	// EditAnywhere : 읽기, 쓰기 모두 가능. 편집o
 	// VisibleInstanceOnly : 읽기 가능. 디폴트 블루프린트에서는 확인할 수 없고 레벨에 인스턴스로 존재하는 디테일패널에서 읽기 가능.
+	// VisibleDefaultsOnly : 디폴트 블루프린트에서만 읽기 가능.
+	// EditDefaultsOnly : 디폴트 블루프린트에서만 읽기, 쓰기 가능.
+	// EditInstanceOnly : 인스턴스에서만 읽기, 쓰기 가능.
+	// BlueprintReadWrite : 이벤트 그래프에서 호출 및 편집 가능.
+	// BlueprintReadOnly : 이벤트 그래프에서 호출 가능, 편집x.
 
-	UPROPERTY(EditAnywhere)
+	// PROPERTY()의 메크로 특징
+	// 읽기 및 쓰기 액세스가 있고, private 섹션에서는 사용할 수 없다.
+	// private 지정자에서 데이터를 읽거나 쓰고 싶은경우 인자로 meta = (AllowPrivateAccess = "true")를 넣으면 사용가능하다.
+	// Category를 넣을 수 있는데 [Category = "Category Name"] 이런식으로 인자값을 넣어주면 블루프린트 에디터에서 해당 카테고리 이름으로 구분이 생긴다.
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float speed = 400.f;
+
+	UPROPERTY(VisibleDefaultsOnly)
+	int32 VisibleDeafultsOnlyInt = 5;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	int32 VisibleAnywhereInt = 5;
+
 
 };
---
+++ Source/ToonTanks/tank.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "tank.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+Atank::Atank()
+{
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Sprint Arm"));
+	SpringArm->SetupAttachment(RootComponent);
+
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+}
